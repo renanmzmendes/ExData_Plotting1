ecec18dd73064a6e06e5e947840d85352e047f1c
Renan Mendes <renanmzmendes@gmail.com>
2014-06-05 23:32:58 -0300

    more comments on second plot

---
+++ plot2.R
@@ -0,0 +1,11 @@
+# Reading and subsetting the data  
+consumption <- read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings="?")
+consumption <- consumption[consumption$Date == "1/2/2007" | consumption$Date == "2/2/2007",]
+consumption <- consumption[complete.cases(consumption),]
+
+# Plotting to PNG image
+# X-axis is created by the concatenation of both date and time
+# and their transformation to a POSIX object
+png()
+plot(strptime(paste(consumption$Date, consumption$Time), format="%d/%m/%Y %H:%M:%S", tz="UTC"), consumption$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
